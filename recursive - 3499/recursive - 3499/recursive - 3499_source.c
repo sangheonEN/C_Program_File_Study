e99b6bcd5654cc395c5c8f19a6be2c95af456627
sangheonEN <jteks6@gmail.com>
2020-08-03 15:14:10 +0900

    1th - commit

---
+++ recursive - 3499/recursive - 3499/recursive - 3499_source.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+int N = 0;
+int count = 0;
+int list = 0;
+
+char arr[20] = { 0, };
+char firstarr[20] = { 0, };
+char secondarr[20] = { 0, };
+char resultarr[20] = { 0, };
+
+void divide(int N, char* arr);
+void mix(int list, char* firstarr, char* secondarr);
+
+int main(void)
+{
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%s", &arr[i]);
+	}
+
+	divide(N, arr);
+
+	mix(0, firstarr, secondarr);
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("%c", resultarr[i]);
+	}
+	return 0;
+}
+
+void divide(int N, char* arr)
+{
+	if (N % 2 != 0)
+	{
+		for (int i = 0; i < (N / 2) + 1; i++)
+		{
+			firstarr[i] = arr[i];
+		}
+		for (int i = (N / 2) + 1; i < N; i++)
+		{
+			secondarr[i] = arr[i];
+		}
+	}
+	else
+	{
+		for (int i = 0; i < N / 2; i++)
+		{
+			firstarr[i] = arr[i];
+		}
+		for (int i = N / 2; i < N; i++)
+		{
+			secondarr[i] = arr[i];
+		}
+	}
+}
+
+void mix(int list, char* firstarr, char* secondarr)
+{
+	if (N = list)
+	{
+		return;
+	}
+	if (firstarr[list] != "")
+	{
+		resultarr[list] = firstarr[list];
+	}
+	if (secondarr[list] != "")
+	{
+		resultarr[list] = secondarr[list];
+	}
+	mix(list + 1, N, firstarr, secondarr);
+}
+
+
+//void mix(int count, int N)
+//{
+//	if (N = count)
+//	{
+//		return;
+//	}
+//	if(N % 2 == 0)
+//	{
+//		for (int i = 0; i < N / 2;)
+//		{
+//			resultarr[i] = firstarr[i];
+//		}
+//		for (int i = N / 2; i < N; i++)
+//		{
+//			resultarr[i] = secondarr[i];
+//		}
+//	}
+//	else
+//	{
+//		for (int i = 0; i < (N / 2)+1;)
+//		{
+//			resultarr[i] = firstarr[i];
+//		}
+//		for (int i = (N / 2) + 1; i < N; i++)
+//		{
+//			resultarr[i] = secondarr[i];
+//		}
+//	}
+//}

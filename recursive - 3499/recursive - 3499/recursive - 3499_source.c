ffb4aff6a61c41a385a5f49b32099f30f83a1261
sangheonEN <jteks6@gmail.com>
2020-08-06 19:15:02 +0900

    recursive_3499_source_2th

--- recursive - 3499/recursive - 3499/recursive - 3499_source.c
+++ recursive - 3499/recursive - 3499/recursive - 3499_source.c
@@ -1,95 +1,103 @@
 	//// 3499. [S/W 문제해결 기본] 7일차 - 퍼펙트 셔플
 
 //case 1)
-//#include <stdio.h>
-//#include <string.h>
-//
-//int N = 0;
-//int count = 0;        
-//
-//char arr[20] = { 0, };
-//char firstarr[20] = { 0, };
-//char secondarr[20] = { 0, };
-//char resultarr[20] = { 0, };
-//
-//void divide(int N, char* arr);
-//void mix(int N);
-//
-//int main(void)
-//{
-//    scanf("%d", &N);
-//
-//    for (int i = 0; i < N; i++)
-//    {
-//        scanf("%s", &arr[i]);
-//    }
-//
-//    divide(N, arr);
-//
-//    memmove(secondarr - (N / 2), secondarr, sizeof(char) * N);
-//
-//    mix(N);
-//
-//
-//    for (int i = 0; i < N; i++)
-//    {
-//        printf("%c\n", resultarr[i]);
-//    }
-//    return 0;
-//}
-//
-//void divide(int N, char* arr)
-//{
-//    if (N % 2 != 0)
-//    {
-//        for (int i = 0; i < (N / 2) + 1; i++)
-//        {
-//            firstarr[i] = arr[i];
-//        }
-//        for (int i = (N / 2) + 1; i < N; i++)
-//        {
-//            secondarr[i] = arr[i];
-//        }
-//    }
-//    else
-//    {
-//        for (int i = 0; i < N / 2; i++)
-//        {
-//            firstarr[i] = arr[i];
-//        }
-//        for (int i = N / 2; i < N; i++)
-//        {
-//            secondarr[i] = arr[i];             // 
-//        }
-//    }
-//}
-//
-//
-//// firstarr[i] A배열      secondarr  B배열
-//// resultarr 배열에 저장하자.
-//// 
-//
-//void mix(int N)
-//{
-//    int i = 0;
-//    if (N <= 0)
-//    {
-//        return;
-//    }
-//    else
-//    {
-//        if (N % 2 == 0)
-//        {
-//            resultarr[N - 1] = secondarr[(N/2) - 1];
-//        }
-//        else
-//        {
-//            resultarr[N - 1] = firstarr[N/2];
-//        }
-//    }
-//    mix(N - 1);
-//}
-//
+#include <stdio.h>
+#include <string.h>
+
+int N = 0;
+int count = 0;        
+
+char arr[20] = { 0, };
+char firstarr[20] = { 0, };
+char secondarr[20] = { 0, };
+char resultarr[20] = { 0, };
+
+void divide(int N, char* arr);
+void mix(int N);
+
+int main(void)
+{
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%s", &arr[i]);
+    }
+
+    divide(N, arr);
+
+    if (N % 2 == 0)
+    {
+        memmove(secondarr - (N / 2), secondarr, sizeof(char) * N);
+    }
+    else
+    {
+        memmove(secondarr - ((N / 2)+1), secondarr, sizeof(char) * (N/2));
+    }
+    mix(N);
+
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%c\n", resultarr[i]);
+    }
+    return 0;
+}
+
+void divide(int N, char* arr)
+{
+    if (N % 2 != 0)
+    {
+        for (int i = 0; i < (N / 2) + 1; i++)
+        {
+            firstarr[i] = arr[i];
+        }
+        for (int i = (N / 2) + 1; i < N; i++)
+        {
+            secondarr[i] = arr[i];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < N / 2; i++)
+        {
+            firstarr[i] = arr[i];
+        }
+        for (int i = N / 2; i < N; i++)
+        {
+            secondarr[i] = arr[i];             // 
+        }
+    }
+}
+
+
+// firstarr[i] A배열      secondarr  B배열
+// resultarr 배열에 저장하자.
+// 
+
+void mix(int N)
+{
+    if (N <= 0)
+    {
+        return;
+    }
+    else
+    {
+        if (N % 2 == 0)
+        {
+          /*  resultarr[N - 1] = secondarr[(N/2) - 1]; */// 짝수
+            resultarr[N - 1] = secondarr[(N / 2) + 3]; // 홀수
+        }
+        else
+        {
+            resultarr[N - 1] = firstarr[N/2];
+        }
+    }
+    printf("N:%d, firstarr:%s\n",N,firstarr);
+    printf("N:%d, secondarr:%s\n",N, secondarr);
+    mix(N - 1);
+}
+
 //
 
 //// case 2)

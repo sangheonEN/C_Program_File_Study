83f3524c68d364e2ede71e83263d4efee54cb973
sangheonEN <jteks6@gmail.com>
2020-08-22 00:07:34 +0900

    hanoi_final

--- hanoi/hanoi/hanoi.c
+++ hanoi/hanoi/hanoi.c
@@ -3,6 +3,8 @@
 int start = 1;
 int middle = 2;
 int target = 3;
+int arr_start[100000000] = { 0, };
+int arr_target[100000000] = { 0, };
 unsigned long long count = 0;
 
 void hanoi_move(int N, int start, int middle, int target);   // 원판의 개수가 짝수 
@@ -13,12 +15,22 @@ int main(void)
 
 	scanf("%d", & N);
 
+	//arr_start= malloc(sizeof(unsigned long long));
+	//arr_target= malloc(sizeof(unsigned long long));
+
+	hanoi_move(N, 1, 2, 3);
+
+	printf("%llu\n", count);
+
 	if (N < 20)
 	{
-		hanoi_move(N, 1, 2, 3);
+		for (int i = 0; i < count; i++)
+		{
+			printf("%d %d\n", arr_start[i], arr_target[i]);
+		}
+
 	}
 
-	printf("%llu", count);
 	return 0;
 
 }
@@ -32,7 +44,9 @@ void hanoi_move(int N, int start, int middle, int target)
 	else
 	{
 		hanoi_move(N - 1, start, target, middle);
-		printf("%d %d\n", start, target);
+		//printf("%d %d\n", start, target);
+		arr_start[count] = start;
+		arr_target[count] = target;
 		count++;
 		hanoi_move(N - 1, middle, start, target);
 		//hanoi_move(N - 1, )
